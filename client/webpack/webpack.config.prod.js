18747f36b110ee3ea9f87e31e6ece46e98d872bf
riley johnson <riley johnson>
2016-12-18 12:36:33 -0500

    fixed redux production problem

--- client/webpack/webpack.config.prod.js
+++ client/webpack/webpack.config.prod.js
@@ -39,7 +39,7 @@ module.exports = _.extend({}, config, {
       }
     }),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      'process.env.NODE_ENV': JSON.stringify('production'),
       '__DEV__': JSON.stringify(false)
     })
   ],
