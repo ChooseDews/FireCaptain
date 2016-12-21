be718df0b1d8c4d7f9a38741c44a1d8769c13cf2
John Dews <John@thedews.us>
2016-12-21 17:57:37 -0500

    improved Continous Dev Server

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -15,6 +15,7 @@ module.exports = function(grunt) {
     grunt.initConfig({
         concat_css: {
             options: {
+              debugMode: false
                 // Task-specific options go here.
             },
             all: {
