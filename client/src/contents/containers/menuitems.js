ba3e2f46636cb0dd8a5271ed42e5fc792260b469
rileyjohnson31 <rileymillerjohnson@gmail.com>
2016-12-27 14:41:43 -0500

    fixed sidenav

---
+++ client/src/contents/containers/menuitems.js
@@ -0,0 +1,34 @@
+const menuItems = [
+	{
+		name: "Home",
+		to: "/"
+	},
+	{
+		name: "Schools",
+		to: "/schools"
+	},
+	{
+		name: "District",
+		to: "/district"
+	},
+	{
+		name: "About",
+		to: "/about"
+	},
+	{
+		name: "Drills",
+		to: "/drills"
+	},
+	{
+		name: "drill link",
+		to: "/specialdrilllink",
+		condition: "drill"
+	},
+	{
+		name: "sudo link",
+		to: "/specialsudolink",
+		condition: "sudo"
+	},
+]
+
+export default menuItems
