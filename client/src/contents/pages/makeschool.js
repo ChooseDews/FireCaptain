c88432fcab7421eb6094d53e285f5343b14076df
John Dews <John@thedews.us>
2016-12-31 12:27:31 -0500

    God this is some awful code

--- client/src/contents/pages/makeschool.js
+++ client/src/contents/pages/makeschool.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Button, Checkbox, Form, Accordion, Icon, Table, Label, Menu, Message } from 'semantic-ui-react'
+import { Button, Checkbox, Form, Accordion, Icon, Table, Label, Menu, Message, Grid, Segment } from 'semantic-ui-react'
 import * as _ from "lodash"
 
 //temp variable for what a schools period setup would look like
@@ -34,7 +34,8 @@ export default class MakeSchool extends React.Component {
 		this.state = {
 			zonesActiveIndex: -1,
 			roomsActiveIndex: -1,
-			zones: []
+			zones: [],
+			activeZone: null
 		}
 	}
 	handleZoneTitleClick(e, i) {
@@ -47,6 +48,23 @@ export default class MakeSchool extends React.Component {
 			roomsActiveIndex: this.state.roomsActiveIndex === i ? -1 : i,
 		})
 	}
+	handleMenuClick(e, {name}){
+		console.log(_.findKey(this.state.zones, {id: name}));
+		this.setState({
+			activeZone: name,
+			zonesActiveIndex: _.findKey(this.state.zones, {id: name})
+		});
+		console.log(this.state.zones);
+	}
+	generateUUID() {
+    var d = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random()*16)%16 | 0;
+        d = Math.floor(d/16);
+        return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+    });
+    return uuid;
+}
 	submit(e) {
 		e.preventDefault()
 		console.log(this.state.zones)
@@ -71,13 +89,218 @@ export default class MakeSchool extends React.Component {
 				      <Checkbox label='random form stuff' name="moreinfocheckbox" />
 				    </Form.Field>
 
+
+						<Grid>
+	<Grid.Column width={2}>
+		<Menu secondary vertical>
+			<Menu.Item name='random'>
+			<b>Zones</b>
+			</Menu.Item>
+			{this.state.zones.map((zone) => {
+
+
+			return ([
+
+				<Menu.Item name={zone.id} content={zone.name} active={this.state.activeZone === zone.id} onClick={this.handleMenuClick.bind(this)} />
+
+			])
+
+			})}
+
+
+			<Menu.Item name='new-tab'>
+	 <Icon name='add' />
+	 <b>Add Zone</b>
+ </Menu.Item>
+
+
+		</Menu>
+	</Grid.Column>
+
+	<Grid.Column stretched width={14}>
+		<Segment>
+			{
+				function(self){
+					console.log(self);
+					if(self.state.zonesActiveIndex < 0) return null;
+					return ([
+							<div>
+
+
+								<Form.Field>
+									<label>Zone Name</label>
+									<input placeholder='Zone Name' value={self.state.zones[self.state.zonesActiveIndex].name} onChange={(e) => {
+											console.log(e.target.value);
+										self.state.zones[self.state.zonesActiveIndex].name = e.target.value;
+										self.setState({
+											zones: self.state.zones
+										})
+									}} />
+								</Form.Field>
+
+
+								<Form.Field>
+									<label>Rooms</label>
+								</Form.Field>
+
+								<Form.Field>
+									<Button type="button" content='Add room' icon='plus' labelPosition='left' onClick={() => {
+									self.state.zones[self.state.zonesActiveIndex].rooms.push({
+										id: self.generateUUID(),
+										name: "New Room",
+										periods: []
+									});
+											self.setState({
+												zones: self.state.zones,
+											})
+									}} />
+								</Form.Field>
+
+
+
+
+								 <Accordion activeIndex={self.state.roomsActiveIndex} onTitleClick={self.handleRoomTitleClick.bind(self)}>
+																    	{self.state.zones[self.state.zonesActiveIndex].rooms.map((room) => {
+																    		return ([
+																    			<Accordion.Title key={room.id}>
+																			      <Icon name='dropdown' />
+																			      {room.name}
+																			      <Button size="mini" icon='remove' color="red" floated="right" style={{marginTop: "-4px", marginRight: "-6px"}} onClick={(e) => {
+																			      	e.stopPropagation()
+																			      	let newZones = self.state.zones
+																			      	newZones.forEach((newZone) => {
+																			      		if (newZone.id == self.state.zones[self.state.zonesActiveIndex].id) {
+																			      			newZone.rooms = newZone.rooms.filter((newRoom) => {
+																			      				return newRoom.id != room.id
+																			      			})
+																			      		}
+																			      	})
+																			      	self.setState({
+																			      		zones: newZones
+																			      	})
+																			      }} />
+																			    </Accordion.Title>,
+																			    <Accordion.Content style={{paddingLeft: "1em"}}>
+
+																			    <Form.Field>
+																			      <label>Room Name</label>
+																			      <input placeholder='Room Name' name={"roomname" + room.id} value={room.name} onChange={(e) => {
+								 													let newZones = self.state.zones
+																			      	newZones.forEach((newZone) => {
+																			      		if (newZone.id == self.state.zones[self.state.zonesActiveIndex].id) {
+																			      			newZone.rooms.forEach((newRoom) => {
+																			      				if (newRoom.id == room.id) {
+																			      					newRoom.name = e.target.value
+																			      				}
+																			      			})
+																			      		}
+																			      	})
+								 													self.setState({
+								 														zones: newZones
+								 													})
+																			      }} />
+																			    </Form.Field>
+
+																			    <Form.Field>
+																			    	<label>Periods</label>
+																			    </Form.Field>
+																			    <Table celled>
+																			      <Table.Header>
+																			        <Table.Row>
+																			          <Table.HeaderCell>Period</Table.HeaderCell>
+																			          <Table.HeaderCell>Status</Table.HeaderCell>
+																			        </Table.Row>
+																			      </Table.Header>
+
+																			      <Table.Body>
+																			      {periods.map((period, periodsIncrement) => {
+																			      	return (
+																			      			<Table.Row key={periodsIncrement}>
+																						      <Table.Cell>{period.name}</Table.Cell>
+																					          <Table.Cell>
+																					          	{room.periods.includes(period.id)?
+																					          		<Checkbox toggle defaultChecked name={"periodtoggle" + periodsIncrement} value={periodsIncrement.toString()} onChange={(e, result) => {
+																					          			let newZones = self.state.zones
+																								      	newZones.forEach((newZone) => {
+																								      		if (newZone.id == self.state.zones[self.state.zonesActiveIndex].id) {
+																								      			newZone.rooms.forEach((newRoom) => {
+																								      				if (newRoom.id == room.id) {
+
+																								      					if (result.checked) {
+																								          				if (newRoom.periods.indexOf(period.id) === -1) {
+																								          					newRoom.periods.push(period.id)
+																								          				}
+																								          			} else {
+																								          				var index = newRoom.periods.indexOf(period.id)
+																								          				if (index > -1) {
+																															newRoom.periods.splice(index, 1);
+																														}
+																								          			}
+
+																								      				}
+																								      			})
+																								      		}
+																								      	})
+																					          			self.setState({
+																					          				zones: newZones
+																					          			})
+																					          		}} />
+																					          		:
+																					          		<Checkbox toggle name={"periodtoggle" + periodsIncrement} value={periodsIncrement.toString()} onChange={(e, result) => {
+																					          			let newZones = self.state.zones
+																								      	newZones.forEach((newZone) => {
+																								      		if (newZone.id == self.state.zones[self.state.zonesActiveIndex].id) {
+																								      			newZone.rooms.forEach((newRoom) => {
+																								      				if (newRoom.id == room.id) {
+
+																								      					if (result.checked) {
+																								          				if (newRoom.periods.indexOf(period.id) === -1) {
+																								          					newRoom.periods.push(period.id)
+																								          				}
+																								          			} else {
+																								          				var index = newRoom.periods.indexOf(period.id)
+																								          				if (index > -1) {
+																															newRoom.periods.splice(index, 1);
+																														}
+																								          			}
+
+																								      				}
+																								      			})
+																								      		}
+																								      	})
+																					          			self.setState({
+																					          				zones: newZones
+																					          			})
+																					          		}} />
+																					          	}
+																					          </Table.Cell>
+																					        </Table.Row>
+																			      		)
+																			      })}
+
+																			      </Table.Body>
+																			    </Table>
+
+																			    </Accordion.Content>
+																    			])
+																    	})}
+
+																	  </Accordion></div>
+					])
+				}(this)
+			}
+		</Segment>
+	</Grid.Column>
+</Grid>
+
+
 				    <h3>Zones</h3>
 
 				    <Form.Field>
 				    	<Button type="button" content='Add zone' icon='plus' labelPosition='left' onClick={() => {
 				    		let newZones = this.state.zones
 				    		newZones.push({
-								id: this.state.zones.length + 1,
+								id: this.generateUUID(),
 								name: "New Zone",
 								rooms: []
 							})
@@ -133,7 +356,7 @@ export default class MakeSchool extends React.Component {
 											      	newZones.forEach((newZone) => {
 											      		if (newZone.id == zone.id) {
 											      			newZone.rooms.push({
-																id: newZone.rooms.length + 1,
+																id: this.generateUUID(),
 																name: "New Room",
 																periods: []
 															})
@@ -179,7 +402,7 @@ export default class MakeSchool extends React.Component {
 												      }} />
 												    </Accordion.Title>,
 												    <Accordion.Content style={{paddingLeft: "1em"}}>
-												    	
+
 												    <Form.Field>
 												      <label>Room Name</label>
 												      <input placeholder='Room Name' name={"roomname" + room.id} value={room.name} onChange={(e) => {
@@ -223,7 +446,7 @@ export default class MakeSchool extends React.Component {
 																	      		if (newZone.id == zone.id) {
 																	      			newZone.rooms.forEach((newRoom) => {
 																	      				if (newRoom.id == room.id) {
-																	      					
+
 																	      					if (result.checked) {
 																	          				if (newRoom.periods.indexOf(period.id) === -1) {
 																	          					newRoom.periods.push(period.id)
@@ -250,7 +473,7 @@ export default class MakeSchool extends React.Component {
 																	      		if (newZone.id == zone.id) {
 																	      			newZone.rooms.forEach((newRoom) => {
 																	      				if (newRoom.id == room.id) {
-																	      					
+
 																	      					if (result.checked) {
 																	          				if (newRoom.periods.indexOf(period.id) === -1) {
 																	          					newRoom.periods.push(period.id)
